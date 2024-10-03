0b2a4c8c0dec77a48b8f9c4b7c771c2cce8ec890
RadowanMahmud <bsse1002@iit.du.ac.bd>
2024-10-03 18:30:02 -0400

    removed extra codes

--- dataset/utils/data_clases.py
+++ dataset/utils/data_clases.py
@@ -221,10 +221,3 @@ class EgoVehicle(Entity):
         corners = np.dot(rot_test.as_matrix(), corners)
 
         return corners
-
-    def bottom_corners(self) -> np.ndarray:
-        """
-        Returns the four bottom corners.
-        :return: <np.float: 3, 4>. Bottom corners. First two face forward, last two face backwards.
-        """
-        return self.corners()[:, [2, 3, 7, 6]]
